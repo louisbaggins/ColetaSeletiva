fef4f0eff0af328d214759cacfd527ea74c11ec4
louisbaggins <44960191+louisbaggins@users.noreply.github.com>
2018-11-12 03:46:43 -0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coleta Seletiva
+//
+//  Created by Luis Araújo on 12/11/2018.
+//  Copyright © 2018 Lista 1. All rights reserved.
+//
+
+#include <string.h>
+#include <iostream>
+#include "residuo.h"
+#include "PontoEntrega.h"
+#include "agendamento.h"
+int main(){
+}
