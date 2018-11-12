0c01757c55143f6cb8b96f98be6c181837892e1f
louisbaggins <44960191+louisbaggins@users.noreply.github.com>
2018-11-12 03:46:09 -0200

    Create Agendamento.h

---
+++ Agendamento.h
@@ -0,0 +1,34 @@
+//
+//  agendamento.h
+//  Coleta Seletiva
+//
+//  Created by Luis Araújo on 12/11/2018.
+//  Copyright © 2018 Lista 1. All rights reserved.
+//
+
+#ifndef agendamento_h
+#define agendamento_h
+#include <string.h>
+#include <iostream>
+#include "Pessoa.h"
+#include "residuo.h"
+#include "PontoEntrega.h"
+/*class Agendamento{
+private:
+public:
+    std::string data;
+    std::string endereco;
+    PessoaJuridica Doador;
+    PessoaJuridica recolhedor;
+    std::string tipoResiduo;
+    bool coleta;
+    Agendamento(std::string data, std::string endereco, PessoaJuridica Doador, PessoaJuridica recolhedor, std::string tipoResiduo){
+        this->data = data;
+        this->endereco = endereco;
+        this->Doador = Doador;
+        this->recolhedor = recolhedor;
+        this->tipoResiduo = tipoResiduo;
+    }
+};*/
+
+#endif /* agendamento_h */
