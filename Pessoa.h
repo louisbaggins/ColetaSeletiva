16beb80534b36c39b0b33ffa336b4556dce9e233
louisbaggins <44960191+louisbaggins@users.noreply.github.com>
2018-12-07 13:08:14 -0200

    Update Pessoa.h

--- Pessoa.h
+++ Pessoa.h
@@ -15,20 +15,39 @@
 class Pessoa{
 private:
 public:
-    std::string tipoResiduo;
-    std::string razao;
-    Pessoa(std::string tipo, std::string razao){
-        this->tipoResiduo = tipo;
-        this->razao = razao;
+    std::string nome;
+    std::string tipo_residuo;
+    ~Pessoa(){
+    }
+    Pessoa (){
+        
+    }
+    Pessoa(std::string nome, std::string tipo){
+        this->nome = nome;
+        this->tipo_residuo = tipo;
+    }
+    virtual std::string gets_res(){
+        return this->tipo_residuo;
+    }
+    
+    virtual std::string gets_nome(){
+        return this->nome;
+    }
+    virtual void set_nome(std::string nome){
+        this->nome = nome;
+    }
+    virtual void set_tipo_residuo(std::string tipo){
+        this->tipo_residuo = tipo;
     }
 };
 class PessoaFisica : public Pessoa {
 private:
 public:
+
     std::string nome;
     std::string CPF;
     //bool VerificaCPF(std::string CPF);
-    PessoaFisica(std::string nome, std::string CPF, std::string tipoResiduo, std::string razao) : Pessoa(tipoResiduo, razao){
+    PessoaFisica(std::string nome, std::string CPF, std::string tipoResiduo, std::string razao ) : Pessoa(tipoResiduo, razao){
         this->nome = nome;
         this->CPF = CPF;
     }
