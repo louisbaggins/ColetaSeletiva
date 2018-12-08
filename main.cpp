46d0e6c144c10a4dfcd074b4d7d44e0fcec92ee4
louisbaggins <44960191+louisbaggins@users.noreply.github.com>
2018-12-08 21:00:50 -0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,5 @@
-//
+///
 //  main.cpp
-//  Coleta Seletiva
-//
-//  Created by Luis Araújo on 12/11/2018.
-//  Copyright © 2018 Lista 1. All rights reserved.
-//
-
-/*#include <string.h>
-#include <iostream>
-#include "residuo.h"
-#include "PontoEntrega.h"
-#include "agendamento.h"
-int main(){
-}*/
-//
-//  Pessoa.h
 //  Coleta Seletiva
 //
 //  Created by Luis Araújo on 12/11/2018.
@@ -22,60 +7,167 @@ int main(){
 //
 #include <string.h>
 #include <iostream>
+#include <vector>
+#include <list>
+#include "Pessoa.h"
 #include "residuo.h"
 #include "PontoEntrega.h"
 #include "agendamento.h"
-#ifndef Pessoa_h
-#define Pessoa_h
-class Pessoa{
-private:
-public:
-    std::string nome;
-    std::string tipo_residuo;
-    ~Pessoa(){
+
+int main() {
+    // insert code here...
+    static std::vector<residuo*> residuos;
+    static std::vector<Pessoa*> teste;
+    std::cout << "ESCOLHA A ACAO QUE DESEJA REALIZAR: \n\n a) Cadastrar residuo \t b) Cadastrar usuario \n c) Cadastrar ponto de entrega\t d) Listar residuos\n e) Listar Usuarios \t f) Listar pontos de entrega" << std::endl;
+    char resposta;
+    std::cin >> resposta;
+    std::string forma_armazenamento;
+    char tipoRes;
+    //Cadastro de residuos(INICIO) -------------------------------------------------------------------------------
+    char cond;
+    std::string res;
+    std::string tipo;
+    std::string quantidade;
+    if(resposta == 'a'|| resposta == 'A'){
+        cond = 's';
+        while(cond == 's' || cond == 'S'){
+            std::cout << "Escolha o tipo de residuo que deseja cadastrar: \na)Liquido\nb)Solido\nc)Organico" << std::endl;
+            std::cin >> tipoRes;
+        if(tipoRes == 'a' || tipoRes == 'A'){
+                std::cout << "Qual o nome do residuo?" << std::endl;
+                std::cin >> res;
+                residuos.push_back(new residuo(res));
+            }
+    if(tipoRes == 'b' || tipoRes == 'B'){
+        std::cout << "Qual o nome do residuo?" << std::endl;
+        std::cin >> res;
+        residuos.push_back(new residuo(res));
     }
-    Pessoa (){
+        if(tipoRes == 'c' || tipoRes == 'C'){
+            std::cout << "Qual o nome do residuo?" << std::endl;
+            std::cin >> res;
+            residuos.push_back(new residuo(res));
+        }
+            std::cout << "\n Deseja realizar o cadastro de mais residuos? \ns) \t n)" << std::endl;
+            std::cin >> cond;
+        }
+        return main();
+}
+     //Cadastro de residuos(FINAL) -------------------------------------------------------------------------------
+   
+    //IMPRESSAO DE RESIDUOS(INICIO) ------------------------------------------------------------------------------
+    if(resposta == 'd' || resposta == 'D'){
+        for(unsigned int contador = 0; contador < residuos.size(); contador++){
+            std::cout << "\n" << residuos[0]->nomeResiduo << std::endl;
+    }
+    char cont;
+    std::cin >> cont;
+    while (cont == 's'){
+        std::cout << "\nDeseja realizar uma nova operação?" << std::endl;
+        std::cin >> cont;
+        return main();
+    }
+        return 0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+    
+    /*//Pessoa *test = new Pessoa("nome", "razao");
+    //std::cout<< "\n" << test->tipo_residuo << std::endl;
+    //std::cout<< "\n" <<test->CPF << std::endl;
+    std::string tipoResiduo;
+    std::string razao;
+    std::cout << "\n" << "Deseja realizar o cadastro pessoas?\n\t S) \t N)" << std::endl;
+    char k;
+    std::cin >> k;
+    while(k == 's' || k == 'S'){
+        std::cout << "\n" << "Digite o tipo de residuo: " << std::endl;
+        std::cin >> tipoResiduo;
+        std::cout << "\n" << "Digite se eh doador ou recolhedor: " << std::endl;
+        std::cin >> razao;
+        teste.push_back(new Pessoa(tipoResiduo, razao));
+        std::cout << "\n" << "Deseja realizar o cadastro de mais pessoas?\n\t S) \t N)" << std::endl;
+        std::cin >> k;
+    }
+        /*for( unsigned int o = 0; o < teste.size(); o++){
+            
+        }
+    
+    
+    if(k == 'n' || k == 'N'){
+    for(unsigned int l = 0; l < teste.size(); l++){
         
+        std::cout << "\n" << teste[l]->gets_res()<< std::endl;
+        std::cout << "\n" << teste[l]->gets_nome()<< std::endl;
+
+
     }
-    Pessoa(std::string nome, std::string tipo){
-        this->nome = nome;
-        this->tipo_residuo = tipo;
+        return 0;
     }
-    virtual std::string gets_res(){
-        return this->tipo_residuo;
+    else{
+        std::cout << "Digite uma letra valida" << std::endl;
     }
     
-    virtual std::string gets_nome(){
-        return this->nome;
-    }
-    virtual void set_nome(std::string nome){
-        this->nome = nome;
-    }
-    virtual void set_tipo_residuo(std::string tipo){
-        this->tipo_residuo = tipo;
-    }
-};
-class PessoaFisica : public Pessoa {
-private:
-public:
+    std::cout << "\n" << teste.size() << std::endl;
+    
+   // std::cout << "\n" << teste[0].razao<< std::endl;
+   
+    PessoaJuridica *P = new PessoaJuridica("Fisico", "Doador","Apple", "123565478");
+    std::cout << "\n" << P->tipoResiduo<< std::endl;
+    std::cout << "\n" << P->CNPJ<< std::endl;
+    std::cout << "\n" << P->nomeFantasia<< std::endl;
+    std::cout << "\n" << P->razao<< std::endl;
+    return 0;
+    return main();
+}*/
+}
 
-    std::string nome;
-    std::string CPF;
-    //bool VerificaCPF(std::string CPF);
-    PessoaFisica(std::string nome, std::string CPF, std::string tipoResiduo, std::string razao ) : Pessoa(tipoResiduo, razao){
-        this->nome = nome;
-        this->CPF = CPF;
-    }
-};
-class PessoaJuridica : public Pessoa {
-private:
-public:
-    std::string nomeFantasia;
-    std::string CNPJ;
-    //bool VerificaCNPJ(std::string CPF);
-    PessoaJuridica(std::string nomeFantasia, std::string CNPJ, std::string tipoResiduo, std::string razao) : Pessoa(tipoResiduo, razao){
-        this->nomeFantasia = nomeFantasia;
-        this->CNPJ = CNPJ;
-    }
-};
-#endif /* Pessoa_h */
