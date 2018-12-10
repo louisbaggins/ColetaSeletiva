9fa520d53544003ec51a4c6329c3c70be409b486
louisbaggins <44960191+louisbaggins@users.noreply.github.com>
2018-12-09 22:46:02 -0200

    Update Agendamento.h

--- Agendamento.h
+++ Agendamento.h
@@ -13,22 +13,40 @@
 #include "Pessoa.h"
 #include "residuo.h"
 #include "PontoEntrega.h"
-/*class Agendamento{
+class Agendamento{
 private:
-public:
-    std::string data;
-    std::string endereco;
-    PessoaJuridica Doador;
-    PessoaJuridica recolhedor;
-    std::string tipoResiduo;
     bool coleta;
-    Agendamento(std::string data, std::string endereco, PessoaJuridica Doador, PessoaJuridica recolhedor, std::string tipoResiduo){
-        this->data = data;
-        this->endereco = endereco;
-        this->Doador = Doador;
+    bool doador;
+    bool receptor;
+    std::string evento;
+    std::string recolhedor;
+    std::string donate;
+public:
+    
+    Agendamento(bool doador, std::string recolhedor, std::string donate){
+        this->doador = doador;
         this->recolhedor = recolhedor;
-        this->tipoResiduo = tipoResiduo;
+        this->donate = donate;
     }
-};*/
-
+    void set_receptor(bool receptor){
+        this->receptor = receptor;
+    }
+    void set_doador(bool doador){
+        this->doador = doador;
+    }
+    std::string get_receptor(){
+       return this->recolhedor;
+    }
+    std::string get_donate(){
+        return this->donate;
+    }
+    void checa_agendamento(){
+        if(this->doador == true && this->receptor == true){
+            coleta = true;
+            std::cout <<"Evento confirmado!" << std::endl;
+        }
+        
+    }
+};
 #endif /* agendamento_h */
+
